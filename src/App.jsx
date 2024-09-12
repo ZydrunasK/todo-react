dd8baf67877d8ee86c3d5b5330d55144b5d06822
Zydrunask <kniukstaz@gmail.com>
2024-09-12 09:08:12 +0300

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Header } from "./components/header/Header"
+import { Footer } from "./components/footer/Footer"
+import { Main } from "./components/main/Main"
+
 export function App() {
-
-
   return (
     <>
-      CONTENT
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
