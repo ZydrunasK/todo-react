901f739d153ea00ae646ea942cbe528ffcc89341
Zydrunask <kniukstaz@gmail.com>
2024-09-14 12:46:20 +0300

    basic moreTasks card popup

--- src/components/tasks/TaskCard.jsx
+++ src/components/tasks/TaskCard.jsx
@@ -1,26 +1,53 @@
+import { useState } from 'react';
 import style from './TaskCard.module.css';
 
 export function TaskCard() {
+    const [moreAct, setMoreAct] = useState('false');
+    const [back, setBack] = useState('false');
+
+    function close() {
+        if (back === 'true') {
+            setMoreAct('false'),
+            setBack('false');
+        }
+    }
+
+    function handleAct() {
+        setMoreAct('true');
+        if (moreAct === 'true') {
+            setMoreAct('false');
+        }
+
+        setBack('true');
+        if (back === 'true') {
+            setBack('false');
+        }
+    }
+
+
     return (
-        <li className={style.task}>
-            <div className={style.header}>
-                <div className={style.more}>...</div>
-                <div className={style.moreActions} data-visible="false">
-                    <button className={style.button} type="button">Move up</button>
-                    <button className={style.button} type="button">Move down</button>
-                    <hr />
-                    <button className={style.button} type="button">In progress</button>
-                    <button className={style.button} type="button">Done</button>
-                    <hr />
-                    <button className={style.button} type="button">Delete</button>
+        <>
+        <div className={style.background} data-visible={ moreAct } onClick={ close }></div>
+            <li className={style.task}>
+                <div className={style.header}>
+                    <div className={style.more} onClick={ handleAct }>...</div>
+                    <div className={style.moreActions} data-visible={ moreAct }>
+                        <button className={style.button} type="button">Move up</button>
+                        <button className={style.button} type="button">Move down</button>
+                        <hr />
+                        <button className={style.button} type="button">In progress</button>
+                        <button className={style.button} type="button">Done</button>
+                        <hr />
+                        <button className={style.button} type="button">Delete</button>
+                    </div>
                 </div>
-            </div>
-            <div className={style.content}>
-                <p>Uzduoties tekstas</p>
-            </div>
-            <div className={style.footer}>
-                <p>Deadline: 2024-09-04</p>
-            </div>
-        </li >
+                <div className={style.content}>
+                    <p>Uzduoties tekstas</p>
+                </div>
+                <div className={style.footer}>
+                    <p>Deadline: 2024-09-04</p>
+                </div>
+            </li >
+        </>
     );
 }
